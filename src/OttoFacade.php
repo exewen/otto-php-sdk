d67bf4b8a0691ef34b3bbfba534e96a6a665d1fa
exewen <exeweb@163.com>
2024-07-26 17:06:23 +0800

    1

--- src/OttoFacade.php
+++ src/OttoFacade.php
@@ -6,11 +6,10 @@ namespace Exewen\Otto;
 use Exewen\Facades\Facade;
 use Exewen\Http\HttpProvider;
 use Exewen\Logger\LoggerProvider;
-use Exewen\Otto\Constants\OttoEnum;
 use Exewen\Otto\Contract\OttoInterface;
 
 /**
- * @method static array getToken(string $clientId, string $clientSecret, string $channel = OttoEnum::CHANNEL)
+ * @method static array getToken(string $clientId, string $clientSecret)
  */
 class OttoFacade extends Facade
 {
